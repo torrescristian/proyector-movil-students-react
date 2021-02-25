7deb1360d10046357e8a293f11f38a9103a168d8
Cristian Torres <cristiantorres@paxicotech.com>
2021-02-25 09:56:01 -0300

    feat: DisplayPage mock

--- src/pages/DisplayPage/index.tsx
+++ src/pages/DisplayPage/index.tsx
@@ -1,5 +1,47 @@
+import CropRotateIcon from '@material-ui/icons/CropRotate';
+import TouchAppIcon from '@material-ui/icons/TouchApp';
+import ZoomInIcon from '@material-ui/icons/ZoomIn';
+import { Display, Img, Instructions, InstructionHighlighted } from './styles';
+
 export default function DisplayPage() {
-    return (
-        <>welcome to display page!</>
-    )
+  const imagePath = 'default-image.gif';
+  const handleClickFullscreen = () => {
+    throw new Error('no implementation yet');
+  };
+
+  return (
+    <Display>
+      <Img
+        className="display__img"
+        src={imagePath}
+        onClick={handleClickFullscreen}
+      />
+      <Instructions>
+        <p>
+          <br />
+          <CropRotateIcon color="primary" />
+          <br />
+          Gire la pantalla para ocultar la barra de navegación
+        </p>
+        <p>
+          <br />
+          <TouchAppIcon color="secondary" />
+          <br />
+          Toque la{' '}
+          <InstructionHighlighted>
+            filmina
+          </InstructionHighlighted>{' '}
+          para verla en pantalla completa.
+        </p>
+        <p>
+          <br />
+          <ZoomInIcon color="action" />
+          <br />
+          <InstructionHighlighted>No</InstructionHighlighted> puede
+          aumentar el tamaño
+          <br /> de la imagen (hacer <em>zoom</em>) en pantalla completa.
+        </p>
+      </Instructions>
+    </Display>
+  );
 }
---
+++ src/pages/DisplayPage/styles.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Display = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr;
+  text-align: center;
+  @media only screen and (orientation: landscape) {
+    grid-template-rows: 1fr;
+  }
+`;
+
+export const Img = styled.img`
+  height: auto;
+  object-fit: contain;
+  width: 100%;
+  @media only screen and (orientation: landscape) {
+    width: auto;
+    height: 100vh;
+    padding: 1vw;
+  }
+`;
+
+export const Instructions = styled.div`
+  @media only screen and (orientation: landscape) {
+    display: none;
+  }
+`;
+
+export const InstructionHighlighted = styled.span`
+  &--highlighted {
+    text-decoration: underline;
+  }
+`;
